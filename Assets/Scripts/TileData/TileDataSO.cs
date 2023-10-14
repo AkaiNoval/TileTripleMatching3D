c204f726db346fa95597a655b0d01ab7e9c5d60e
AkaiNoval <127651185+AkaiNoval@users.noreply.github.com>
2023-10-14 23:56:48 +0700

    + URP

---
+++ Assets/Scripts/TileData/TileDataSO.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="New Tile Data")]
+public class TileDataSO : ScriptableObject
+{
+    public Sprite tileSprite;
+}
