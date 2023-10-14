f7a0022656eef1bf430528549c09e21d7ccf1736
AkaiNoval <127651185+AkaiNoval@users.noreply.github.com>
2023-10-14 20:47:58 +0700

    + Tile Model

--- Assets/Scripts/Managers/TileSpawnManager.cs
+++ Assets/Scripts/Managers/TileSpawnManager.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
-/// DESCRIPTION: Spawnable 300 tiles with 10 radius
+/// DESCRIPTION:
 /// NOTE: If the amountToSpawn is too high, you need to consider increase the spawnRadius or decrease the spawnCollisionCheckRadius
 /// </summary>
 public class TileSpawnManager : MonoBehaviour
 {
     [SerializeField] GameObject tileToSpawn;
     [SerializeField] int amountToSpawn;
-    [SerializeField] float spawnRadius;
+    [SerializeField] float spawnRadiusX;
+    [SerializeField] float spawnRadiusY;
+    [SerializeField] float spawnRadiusZ;
+    [SerializeField] bool shouldDetectCollisionWhenSpawning;
 
     const int maxSpawnAttempts = 100;
     const float spawnCollisionCheckRadius = 1;
@@ -42,13 +45,13 @@ public class TileSpawnManager : MonoBehaviour
 
         while (spawnAttempts < maxSpawnAttempts)
         {
-            Vector3 spawnPoint = transform.position + Random.insideUnitSphere * spawnRadius;
-
-            if (!Physics.CheckSphere(spawnPoint, spawnCollisionCheckRadius))
+            //Vector3 spawnPoint = transform.position + Random.insideUnitSphere * spawnRadius;
+            Vector3 spawnPoint = GetRandomSpawnPoint(spawnRadiusX, spawnRadiusY, spawnRadiusZ);
+            if (!shouldDetectCollisionWhenSpawning || !Physics.CheckSphere(spawnPoint, spawnCollisionCheckRadius))
             {
                 float randomYRotation = RandomTileRotation(0f, 360f);
-                float randomXRotation = RandomTileRotation(-45, 45);
-                float randomZRotation = RandomTileRotation(-45, 45);
+                float randomXRotation = RandomTileRotation(-20, 20);
+                float randomZRotation = RandomTileRotation(-20, 20);
                 Quaternion rotation = Quaternion.Euler(randomXRotation, randomYRotation, randomZRotation);
                 var spawnedTile = Instantiate(tileToSpawn, spawnPoint, rotation);
                 spawnedTile.transform.parent = transform;
@@ -61,4 +64,19 @@ public class TileSpawnManager : MonoBehaviour
         return false;
     }
     float RandomTileRotation(float minRotation, float maxRotation) => Random.Range(minRotation, maxRotation);
+    private Vector3 GetRandomSpawnPoint(float radiusX, float radiusY, float radiusZ)
+    {
+        float randomX = Random.Range(-radiusX, radiusX);
+        float randomY = Random.Range(-radiusY, radiusY);
+        float randomZ = Random.Range(-radiusZ, radiusZ);
+
+        Vector3 randomPoint = transform.position + new Vector3(randomX, randomY, randomZ);
+
+        return randomPoint;
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireCube(transform.position, new Vector3(spawnRadiusX * 2, spawnRadiusY * 2, spawnRadiusZ * 2));
+    }
 }
