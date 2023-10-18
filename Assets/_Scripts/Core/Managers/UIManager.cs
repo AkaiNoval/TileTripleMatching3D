39f185584fd7bcdf762f5ac2b63a308e381c9416
AkaiNoval <127651185+AkaiNoval@users.noreply.github.com>
2023-10-19 03:36:58 +0700

    + Core gameplay complete

--- Assets/_Scripts/Core/Managers/UIManager.cs
+++ Assets/_Scripts/Core/Managers/UIManager.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
+    #region Fields
     [SerializeField] GameObject mainMenuUI;
     [SerializeField] GameObject gameplayUI;
     [SerializeField] GameObject winUI;
-    [SerializeField] GameObject lostUI;
+    [SerializeField] GameObject lostUI; 
+    #endregion
+
+    #region EventListener
     private void Start()
     {
         /* Subscribe to the GameManager's OnGameStateChanged event */
         GameManager.OnGameStateChanged += HandleGameStateChange;
     }
+    private void OnDestroy() => GameManager.OnGameStateChanged -= HandleGameStateChange;
+    #endregion
+
+    #region HandleUI
     private void HandleGameStateChange(GameState newState)
     {
         /* Deactivate all UI elements initially */
@@ -38,7 +46,13 @@ public class UIManager : MonoBehaviour
                 lostUI.SetActive(true);
                 break;
         }
-    }
+    } 
+    #endregion
 
-    private void OnDestroy() => GameManager.OnGameStateChanged -= HandleGameStateChange;
+    #region UI
+    public void GoBackToMainMenuButton()
+    {
+        GameManager.Instance.UpdateGameState(GameState.Menu);
+    } 
+    #endregion 
 }
